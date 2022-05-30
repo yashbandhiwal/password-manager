a32c67b38bc3be4c0248323eccab234b5ec0525d
yash bandhiwal <yash.bandhiwal@gmail.com>
2022-05-30 11:57:23 +0530

    auth of user

--- routes/auth.js
+++ routes/auth.js
@@ -12,8 +12,15 @@ const {
 
 const router = express.Router();
 
-// const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/getMe', protect, getMe);
+router.get('/logout', logout);
+router.post('/updateDetails', protect, updateDetails)
+router.post('/updatePassword', protect, updatePassword);
+router.post('/forgotPassword', forgotPassword)
+router.post('/resetPassword/:resettoken', resetPassword)
 
 module.exports = router;

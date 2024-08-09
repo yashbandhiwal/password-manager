353e148735fdb0622bc31e153833e5c108724267
yashbandhiwal <yash.bandhiwal1995@gmail.com>
2024-08-09 08:15:25 +0530

    auth remove generatepassword

--- routes/manager.js
+++ routes/manager.js
@@ -13,7 +13,7 @@ const router = express.Router();
 const { protect } = require('../middleware/auth');
 
 router.post('/savePassword',protect, savePassword);
-router.post('/generatePassword', protect, generatePassword);
+router.post('/generatePassword', generatePassword);
 router.post('/getall',getall);
 router.route("/getone").post(getOne);
 router.route("/viewpassword").post(viewPassword);
